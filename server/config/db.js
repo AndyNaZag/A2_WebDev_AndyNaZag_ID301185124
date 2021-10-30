11bf2a40f14b6c6d279b54aa593bdf243d9615f4
Andy NaZag <andynava57@gmail.com>
2021-10-30 19:08:26 -0400

    updated db, URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/book_store"
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://Andy:ECLQmkaSQ3Gu7bSE@mongodbserver.irst3.mongodb.net/book_store?retryWrites=true&w=majority"
 }
